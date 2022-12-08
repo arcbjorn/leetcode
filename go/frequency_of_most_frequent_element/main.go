713f6d5c209cce9e6e270bdb9480f8d8ca01d681
arcbjorn <arky.happ@gmail.com>
2022-12-08 16:27:44 -0300

    feat(go): frequency_of_most_frequent_element(1838)

--- go/frequency_of_most_frequent_element/main.go
+++ go/frequency_of_most_frequent_element/main.go
@@ -6,60 +6,36 @@ import (
 )
 
 func maxFrequency(nums []int, k int) int {
-	// key = number, value = frequency (how many types the number is in array)
-	// if need to know the actual number in some case
-	// frequencyMap := make(map[int]int)
+	sort.Ints(nums)
+	maxFreq := 1
+	incrementsUsed := 0
 
-	maxFrequency := 1
+	leftElement, rightElement, sampleLengh := 0, 1, len(nums)
 
-	// descending order
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
+	for rightElement < sampleLengh {
+		incrementsUsed += (nums[rightElement] - nums[rightElement-1]) * (rightElement - leftElement)
+		rightElement++
 
-	// go by each element
-	// for each element increment next element by 1 until it equals the first (target) element
-	// skip last element
-	for i := 0; i < len(nums)-1; i++ {
-		availableIncrements := k
-
-		currentNumber := nums[i]
-
-		// if need to know the actual number in some case
-		// frequencyMap[currentNumber]++
-		localFrequency := 1
-
-		nextIndex := i + 1
-
-		// go until the end of the array and until 0 availableIncrements
-		for nextElementIndex := nextIndex; nextElementIndex < len(nums) && availableIncrements > 0; nextElementIndex++ {
-			// exit if equal, it's a match
-			for nums[nextElementIndex] < currentNumber && availableIncrements > 0 {
-				nums[nextElementIndex]++
-
-				// when numbers match, add frequency & go to the next target element
-				if nums[nextElementIndex] == currentNumber {
-					// if need to know the actual number in some case
-					// frequencyMap[currentNumber]++
-					localFrequency++
-				}
-
-				availableIncrements--
-			}
+		for incrementsUsed > k {
+			incrementsUsed -= nums[rightElement-1] - nums[leftElement]
+			leftElement++
 		}
 
-		if localFrequency > maxFrequency {
-			maxFrequency = localFrequency
-		}
-
+		maxFreq = max(maxFreq, rightElement-leftElement)
 	}
+	return maxFreq
+}
 
-	return maxFrequency
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func main() {
-	sampleArray := []int{3, 9, 6}
-	sampleInt := 2
+	sampleArray := []int{9930, 9923, 9983, 9997, 9934, 9952, 9945, 9914, 9985, 9982, 9970, 9932, 9985, 9902, 9975, 9990, 9922, 9990, 9994, 9937, 9996, 9964, 9943, 9963, 9911, 9925, 9935, 9945, 9933, 9916, 9930, 9938, 10000, 9916, 9911, 9959, 9957, 9907, 9913, 9916, 9993, 9930, 9975, 9924, 9988, 9923, 9910, 9925, 9977, 9981, 9927, 9930, 9927, 9925, 9923, 9904, 9928, 9928, 9986, 9903, 9985, 9954, 9938, 9911, 9952, 9974, 9926, 9920, 9972, 9983, 9973, 9917, 9995, 9973, 9977, 9947, 9936, 9975, 9954, 9932, 9964, 9972, 9935, 9946, 9966}
+	sampleInt := 3056
 
 	maxFreq := maxFrequency(sampleArray, sampleInt)
 	fmt.Println(maxFreq)
